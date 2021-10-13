86aad89d0c4dc594fbb1036a906199b01cbfcb0f
Karol Krzywoń <karokrz849@student.polsl.pl>
2021-10-13 16:44:00 +0200

    nowy projekt z progress barem

---
+++ PGU/Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    public Slider slider;
+    public Text progressText;
+    public float gameTime;
+    public float currentTime;
+    private float progressValue;
+    public float ProgressValue
+    {
+        get { return progressValue; }
+        set
+        {
+            progressValue = value;
+            slider.value = progressValue;
+            string progressString = (progressValue * 100).ToString("0.00") + "%";
+            progressText.text = progressString;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        ProgressValue = 0f;
+        currentTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ProgressValue < 1)
+        {
+            currentTime += Time.deltaTime;
+            ProgressValue = currentTime / (gameTime * 60);
+        } else
+        {
+            ProgressValue = 1;
+        }
+    }
+}
